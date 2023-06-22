f8e614e33bfeefa1e390b978698a9f545d6e597d
sasnigdho <salmanahmedims@gmail.com>
2023-06-23 03:33:36 +0600

    DbService added.

--- lib/app/data/services/db_service.dart
+++ lib/app/data/services/db_service.dart
@@ -1,32 +1,100 @@
-// import 'dart:developer';
+import 'dart:developer';
 
-// import 'package:isar/isar.dart';
-// import 'package:path_provider/path_provider.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 
-// import 'i_db_service.dart';
+import '../models/repository/github_repository.dart';
+import '../models/repository/owner.dart';
+import 'i_db_service.dart';
 
-// class DbService implements IDbService {
-//   late Future<Isar> _db;
+class DbService implements IDbService {
+  late Future<Isar> _db;
 
-//   DbService() {
-//     _db = openDB();
-//   }
+  DbService() {
+    _db = openDB();
+  }
 
-//   Future<Isar> openDB() async {
-//     const dbName = 'collections';
-//     final dir = await getApplicationDocumentsDirectory();
+  Future<Isar> openDB() async {
+    const dbName = 'collections';
+    final dir = await getApplicationDocumentsDirectory();
 
-//     if (Isar.instanceNames.isEmpty) {
-//       return Isar.openSync(
-//         [CartSchema],
-//         directory: dir.path,
-//         name: dbName,
-//         inspector: true,
-//       );
-//     }
+    if (Isar.instanceNames.isEmpty) {
+      return Isar.openSync(
+        [GithubRepositorySchema, OwnerSchema],
+        directory: dir.path,
+        name: dbName,
+        inspector: true,
+      );
+    }
 
-//     return Future.value(Isar.getInstance(dbName));
-//   }
+    return Future.value(Isar.getInstance(dbName));
+  }
 
-  
-// }
+  @override
+  Future<void> delete(GithubRepository repository) async {
+    try {
+      final isar = await _db;
+
+      await isar.writeTxn(() async {
+        await isar.owners.delete(repository.owner!.id!);
+        await isar.githubRepositorys.delete(repository.id!);
+      });
+    } catch (e) {
+      log('Error => DbService:: delete@ $e');
+    }
+  }
+
+  @override
+  Future<GithubRepository?> getRepository(int id) async {
+    try {
+      final isar = await _db;
+
+      final repository =
+          await isar.githubRepositorys.filter().idEqualTo(id).findFirst();
+      await repository?.ownerLink.load();
+
+      return repository;
+    } catch (e) {
+      log('Error => DbService:: getRepositories@ $e');
+      return null;
+    }
+  }
+
+  @override
+  Future<List<GithubRepository>> getRepositories() async {
+    final repositories = <GithubRepository>[];
+
+    try {
+      final isar = await _db;
+      repositories.addAll(await isar.githubRepositorys.where().findAll());
+
+      for (final element in repositories) {
+        await element.ownerLink.load();
+      }
+
+      return repositories;
+    } catch (e) {
+      log('Error => DbService:: getRepositories@ $e');
+      return repositories;
+    }
+  }
+
+  @override
+  Future<void> save(GithubRepository repository) async {
+    try {
+      final isar = await _db;
+
+      await isar.writeTxn(() async {
+        final owner = repository.owner;
+        repository.ownerLink.value = owner;
+
+        await isar.owners.put(owner!);
+
+        await isar.githubRepositorys.put(repository);
+        await repository.ownerLink.save();
+      });
+    } catch (e) {
+      log('Error => DbService:: getRepositories@ $e');
+    }
+  }
+}
--- lib/app/data/services/i_db_service.dart
+++ lib/app/data/services/i_db_service.dart
@@ -3,6 +3,6 @@ import 'package:flutter_github_view/app/data/models/repository/github_repository
 abstract class IDbService {
   Future<List<GithubRepository>> getRepositories();
   Future<void> save(GithubRepository repository);
-  Future<GithubRepository?> getCart(int id);
+  Future<GithubRepository?> getRepository(int id);
   Future<void> delete(GithubRepository repository);
 }
