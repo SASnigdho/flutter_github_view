80d921d9a41b79afc240915c4cd0828094e9c0ee
sasnigdho <salmanahmedims@gmail.com>
2023-06-22 23:15:57 +0600

    Page navigation added in SplashController.

--- lib/app/controllers/splash_controller.dart
+++ lib/app/controllers/splash_controller.dart
@@ -1,3 +1,15 @@
+import 'package:get/get.dart';
+
+import '../routes/route_names.dart';
 import 'main_controller.dart';
 
-class SplashController extends MainController {}
+class SplashController extends MainController {
+  @override
+  Future<void> onInit() async {
+    super.onInit();
+
+    await Future.delayed(const Duration(seconds: 2), () async {
+      await Get.offAllNamed(RouteNames.home);
+    });
+  }
+}
