4777e8de415078a274c6c36f7c6198291d6f8466
sasnigdho <salmanahmedims@gmail.com>
2023-06-22 23:11:06 +0600

    Splash page, controller and binding added.

---
+++ lib/app/ui/pages/splash_page/splash_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../../../controllers/splash_controller.dart';
+
+class SplashPage extends GetView<SplashController> {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('GitHub')),
+    );
+  }
+}
