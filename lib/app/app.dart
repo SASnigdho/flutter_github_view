96921db282cdd33bb53adf9a0028d181c8979399
sasnigdho <salmanahmedims@gmail.com>
2023-06-22 23:03:28 +0600

    App file added.

---
+++ lib/app/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'const/app_colors.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
