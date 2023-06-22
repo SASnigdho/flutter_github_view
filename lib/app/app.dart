082a9d93d030d7c24e700fba880e28fe12e9b997
sasnigdho <salmanahmedims@gmail.com>
2023-06-22 23:15:40 +0600

    App file update.

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
+import 'routes/app_pages.dart';
+import 'routes/route_names.dart';
 import 'ui/theme/app_theme.dart';
 
 class App extends StatelessWidget {
@@ -7,8 +10,15 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      // Routing.
+      initialRoute: RouteNames.splash,
+      getPages: AppPages.pages,
+      builder: (_, child) => child!,
+      unknownRoute: AppPages.pages.first,
+
       theme: AppTheme.light,
     );
   }
