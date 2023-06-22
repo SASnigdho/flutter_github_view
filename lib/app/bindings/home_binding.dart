b64a3a3c2a9799a30b8ce7e436d9dc9c7c92e3ad
sasnigdho <salmanahmedims@gmail.com>
2023-06-22 23:25:02 +0600

    Api client inject in HomeBinding.

--- lib/app/bindings/home_binding.dart
+++ lib/app/bindings/home_binding.dart
@@ -1,11 +1,12 @@
-
 import 'package:get/get.dart';
 import '../controllers/home_controller.dart';
-
+import '../data/helpers/api_client.dart';
 
 class HomeBinding implements Bindings {
   @override
   void dependencies() {
     Get.lazyPut<HomeController>(() => HomeController());
+
+    Get.lazyPut(() => ApiClient(), fenix: true);
   }
 }
